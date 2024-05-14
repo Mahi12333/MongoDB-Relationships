6da041415e83a9ddeb6fbb18c898da7eae812810
Mahitosh Giri <88604119+Mahi12333@users.noreply.github.com>
2024-05-14 13:19:38 +0530

    Delete routes directory

--- routes/oneToManyRoute.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createAuthorWithBooks,getAuthorWithBooks } from "../controller/RelationShip/oneToManyController.js";
-const router=Router();
-
-router.post('/oneMany',createAuthorWithBooks );
-
-export default router;
